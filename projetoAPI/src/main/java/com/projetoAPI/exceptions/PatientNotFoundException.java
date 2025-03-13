79d5db10f70c1be4b70d9a397188c5655036e690
Victor77-coder <victor_medeiros77@icloud.com>
2025-03-13 18:37:53 -0300

    Delete projetoAPI directory

--- projetoAPI/src/main/java/com/projetoAPI/exceptions/PatientNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.projetoAPI.exceptions;
-
-public class PatientNotFoundException extends RuntimeException { // Exceção para paciente não encontrado
-    public PatientNotFoundException(String message) {
-        super(message);
-    } // Mensagem de erro
-}
