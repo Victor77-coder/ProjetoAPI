fa7a89d9b7275a059a5086943d5a6e804efbb708
Victor77-coder <victor_medeiros77@icloud.com>
2025-03-13 17:40:01 -0300

    Delete projetoAPI directory

--- projetoAPI/src/main/java/com/projetoAPI/exceptions/AppointmentNotAvailableException.java
+++
@@ -1,7 +0,0 @@
-package com.projetoAPI.exceptions;
-
-public class AppointmentNotAvailableException extends RuntimeException { // Exceção para consultas não disponíveis
-    public AppointmentNotAvailableException(String message) {
-        super(message); // Mensagem de erro
-    }
-}
